8a602889740fac23464c089a6b0483bc4f87c404
Adam Seid Tahir <adam.seidtahir@hotmail.com>
2019-09-15 22:50:38 +0200

    First draft

---
+++ script.js
@@ -0,0 +1,147 @@
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
+TweenMax.from('#umbilical', 1.5, {opacity:0, rotation: -30, delay: 0.5, ease: Power4.easeOut});
+TweenMax.from('#logo', 1, {opacity:0, delay: 1, ease: Power2.easeIn});
+TweenMax.from('#rightNav', 1, {left:200, delay: 1.7, ease: Power2.easeOut});
+TweenMax.from('#leftNav', 1, {left:-200, delay: 1.7, ease: Power2.easeOut});
+TweenMax.from('#arrow', 1, {top: "85%", opacity:0, delay: 2, ease: Power2.easeOut})
+
+// overview text scrolling
+var overTopics = document.querySelector('.overTopics').cloneNode(true);
+document.getElementById("overRoll").appendChild(overTopics);
+TweenMax.to('.overTopics',20,{x:-overTopics.offsetWidth,repeat:-1,ease:Linear.easeNone});
+
+// concept text scrolling
+var conceptTopics = document.querySelector('.conceptTopics').cloneNode(true);
+document.getElementById("conceptRoll").appendChild(conceptTopics);
+TweenMax.to('.conceptTopics',20,{x:-conceptTopics.offsetWidth,repeat:-1,ease:Linear.easeNone});
+
+// narrative text scrolling
+var narrativeTopics = document.querySelector('.narrativeTopics').cloneNode(true);
+document.getElementById("narrativeRoll").appendChild(narrativeTopics);
+TweenMax.to('.narrativeTopics',24,{x:-narrativeTopics.offsetWidth,repeat:-1,ease:Linear.easeNone});
+
+// performance text scrolling
+var performanceTopics = document.querySelector('.performanceTopics').cloneNode(true);
+document.getElementById("performanceRoll").appendChild(performanceTopics);
+TweenMax.to('.performanceTopics',30,{x:-performanceTopics.offsetWidth,repeat:-1,ease:Linear.easeNone});
+
+// team text scrolling
+var teamTopics = document.querySelector('.teamTopics').cloneNode(true);
+document.getElementById("teamRoll").appendChild(teamTopics);
+TweenMax.to('.teamTopics',22,{x:-teamTopics.offsetWidth,repeat:-1,ease:Linear.easeNone});
+
+// contact text scrolling
+var contactTopics = document.querySelector('.contactTopics').cloneNode(true);
+document.getElementById("contactRoll").appendChild(contactTopics);
+TweenMax.to('.contactTopics',27,{x:-contactTopics.offsetWidth,repeat:-1,ease:Linear.easeNone});
+
+
+
+
+
+
+//Logo animations
+var controller = new ScrollMagic.Controller();
+var tl1 = new TimelineMax();
+tl1.add(TweenMax.to("#logo", 1, {width:"6vw", top: "5%", left: "3%", marginTop: 0}));
+tl1.to('#arrow', 1, {opacity:0}, "-=1")
+tl1.to('#umbilical', 2, {opacity:0, ease: Power2.easeIn}, "-=2");
+// tl1.to("#logo", 5, { rotation:"360", ease:Linear.easeNone, repeat:-1}, "-=1");
+tl1.to('#logo', 1, {delay: 1, attr:{src:"img/logoMini.png"}}, "-=1");
+
+
+
+
+var scene = new ScrollMagic.Scene({
+  triggerElement: "#trigger1",
+  triggerHook: 0.9
+})
+  .setTween(tl1)
+  .addTo(controller);
+
+
+
+//Block animations
+
+var tl1b = TweenMax.to("#logo", 5, { rotation:"360", ease:Linear.easeNone, repeat:-1}, "-=1");
+
+var scene = new ScrollMagic.Scene({
+  triggerElement: "#trigger1",
+  triggerHook: 0.9
+})
+  .setTween(tl1b)
+  .addTo(controller)
+  .reverse(false);
+
+
+var tl2 = TweenMax.to('#quo1', 2, {opacity:1});
+
+var scene = new ScrollMagic.Scene({
+  triggerElement: "#quo1",
+  triggerHook: 0.7
+})
+  .setTween(tl2)
+  .addTo(controller);
+
+
+var tl3 = TweenMax.to('#overview', 2, {opacity:1});
+
+var scene = new ScrollMagic.Scene({
+  triggerElement: "#overview",
+  triggerHook: 0.7
+})
+  .setTween(tl3)
+  .addTo(controller);
+
+
+var tl4 = TweenMax.to('#concept', 2, {opacity:1});
+
+var scene = new ScrollMagic.Scene({
+  triggerElement: "#concept",
+  triggerHook: 0.7
+})
+  .setTween(tl4)
+  .addTo(controller);
+
+
+var tl5 = TweenMax.to('#narrative', 2, {opacity:1});
+
+var scene = new ScrollMagic.Scene({
+  triggerElement: "#narrative",
+  triggerHook: 0.7
+})
+  .setTween(tl5)
+  .addTo(controller);
+
+
+var tl6 = TweenMax.to('#performance', 2, {opacity:1});
+
+var scene = new ScrollMagic.Scene({
+  triggerElement: "#performance",
+  triggerHook: 0.7
+})
+  .setTween(tl6)
+  .addTo(controller);
+
+
+var tl7 = TweenMax.to('#team', 2, {opacity:1});
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: "#team",
+    triggerHook: 0.7
+})
+    .setTween(tl7)
+    .addTo(controller);
+
+
+var tl8 = TweenMax.to('#contact', 2, {opacity:1});
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: "#contact",
+    triggerHook: 0.7
+})
+    .setTween(tl8)
+    .addTo(controller);
