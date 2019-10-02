82ef82599b742a121a2486b670adec902d9b74c7
Adam Seid Tahir <adam.seidtahir@hotmail.com>
2019-10-02 11:46:05 +0200

    Quick cleaning and commenting of js file

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
+// Return to top of page on refresh/update
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 }
 
+
+// Intro animation
 TweenMax.from('#umbilical', 1.5, {opacity:0, rotation: -30, delay: 0.5, ease: Power4.easeOut});
 TweenMax.from('#logo', 1, {opacity:0, delay: 1, ease: Power2.easeIn});
 TweenMax.from('#rightNav', 1, {left:200, delay: 1.7, ease: Power2.easeOut});
 TweenMax.from('#leftNav', 1, {left:-200, delay: 1.7, ease: Power2.easeOut});
 TweenMax.from('#arrow', 1, {top: "85%", opacity:0, delay: 2, ease: Power2.easeOut});
 
-//Logo animations
+// Animation on the top section on first scroll. 
 var controller = new ScrollMagic.Controller();
 var tl1 = new TimelineMax();
 tl1.add(TweenMax.to("#logo", 1, {width:"6vw", top: "5%", left: "3%", marginTop: 0}));
 tl1.to('#arrow', 1, {opacity:0}, "-=1");
 tl1.to('#umbilical', 2, {opacity:0, ease: Power2.easeIn}, "-=2");
-// tl1.to("#logo", 5, { rotation:"360", ease:Linear.easeNone, repeat:-1}, "-=1");
 tl1.to('#logo', 1, {delay: 1, attr:{src:"img/logoMini.png"}}, "-=1");
-
-
-
 
 var scene = new ScrollMagic.Scene({
   triggerElement: "#trigger1",
@@ -29,8 +28,8 @@ var scene = new ScrollMagic.Scene({
 
 
 
-//Block animations
 
+// Make the logo rotate one time
 var tl1b = TweenMax.to("#logo", 5, { rotation:"360", ease:Linear.easeNone, repeat:0}, "-=1"); //Set repeat to "-1" for infinite
 
 var scene = new ScrollMagic.Scene({
@@ -42,6 +41,9 @@ var scene = new ScrollMagic.Scene({
   .reverse(false);
 
 
+
+//Block animations that turn every block visible when trigger is hit. 
+//Should have had better variable names... Woops sorry!
 var tl2 = TweenMax.to('#quo1', 3, {opacity:1});
 
 var scene = new ScrollMagic.Scene({
@@ -52,8 +54,7 @@ var scene = new ScrollMagic.Scene({
   .addTo(controller);
 
 
-var tl3 = new TimelineMax();
-tl3.add(TweenMax.to('#overview', 3, {opacity:1}));
+var tl3 = TweenMax.to('#overview', 3, {opacity:1});
 
 var scene = new ScrollMagic.Scene({
   triggerElement: "#overview",
@@ -63,8 +64,7 @@ var scene = new ScrollMagic.Scene({
   .addTo(controller);
 
 
-var tl4 = new TimelineMax();
-tl4.add(TweenMax.to('#concept', 3, {opacity:1}));
+var tl4 = TweenMax.to('#concept', 3, {opacity:1});
 
 var scene = new ScrollMagic.Scene({
   triggerElement: "#concept",
@@ -74,9 +74,7 @@ var scene = new ScrollMagic.Scene({
   .addTo(controller);
 
 
-
-var tl5 = new TimelineMax();
-tl5.add(TweenMax.to('#narrative', 3, {opacity:1}));
+var tl5 = TweenMax.to('#narrative', 3, {opacity:1});
 
 var scene = new ScrollMagic.Scene({
   triggerElement: "#narrative",
@@ -86,8 +84,7 @@ var scene = new ScrollMagic.Scene({
   .addTo(controller);
 
 
-var tl6 = new TimelineMax();
-tl6.add(TweenMax.to('#performance', 3, {opacity:1}));
+var tl6 = TweenMax.to('#performance', 3, {opacity:1});
 
 var scene = new ScrollMagic.Scene({
   triggerElement: "#performance",
@@ -97,8 +94,7 @@ var scene = new ScrollMagic.Scene({
   .addTo(controller);
 
 
-var tl7 = new TimelineMax();
-tl7.add(TweenMax.to('#team', 3, {opacity:1}));
+var tl7 = TweenMax.to('#team', 3, {opacity:1});
 
 var scene = new ScrollMagic.Scene({
     triggerElement: "#team",
@@ -108,8 +104,7 @@ var scene = new ScrollMagic.Scene({
     .addTo(controller);
 
 
-var tl8 = new TimelineMax();
-tl8.add(TweenMax.to('#contact', 3, {opacity:1}));
+var tl8 = TweenMax.to('#contact', 3, {opacity:1});
 
 var scene = new ScrollMagic.Scene({
     triggerElement: "#contact",
